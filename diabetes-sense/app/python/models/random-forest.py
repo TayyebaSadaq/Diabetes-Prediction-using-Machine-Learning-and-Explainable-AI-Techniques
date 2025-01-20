c8989d1a902381c9398f7de0bf2e1db4473263c3
TayyebaSadaq <official.tayyeba@gmail.com>
2025-01-20 19:49:57 +0000

    pre-processing data: - handling null and zero values -> filled with mean calculations (sklearn and simple inputter) - checked if data was balanced/imbalanced -> was imbalanced so used upsampling

--- diabetes-sense/app/python/models/random-forest.py
+++ diabetes-sense/app/python/models/random-forest.py
@@ -2,7 +2,10 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.impute import SimpleImputer # for handling missing values
+from sklearn.utils import resample # for handling imbalanced data
 
+''' IMPORTING DATA '''
 data = pd.read_csv('app\data\pima-indians-diabetes.csv')
 
 ''' OBSERVING DATA - SEE JOURNAL NOTES FOR MORE DETAILS '''
@@ -13,9 +16,33 @@ data = pd.read_csv('app\data\pima-indians-diabetes.csv')
 ''' DATA PREPROCESSING '''
 #sns.heatmap(data.isnull(),cbar = False, cmap = 'magma') # Checking for missing values
 #print(data.isnull().sum())
-''' were null values replaced with 0s? '''
-error = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin','BMI', 'DiabetesPedigreeFunction']
-print(data[error].isin([0]).sum())
 
-data[error] = data[error].replace(0, np.NaN)
-print(data.isnull().sum())
+''' were null values replaced with 0s? - handling this '''
+error = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin','BMI', 'DiabetesPedigreeFunction']
+#print(data[error].isin([0]).sum())
+
+data[error] = data[error].replace(0, np.NaN) # temporarily replace 0s with NaN
+#print(data.isnull().sum())
+
+''' impute null values and fill with mean '''
+si = SimpleImputer(missing_values=np.nan, strategy='mean')
+data[error] = si.fit_transform(data[error])
+
+'''checking null values are fixed - yes'''
+#print(data.isnull().sum())
+
+''' is data balanced or imbalanced? - imbalanced'''
+sns.countplot(data['Outcome'])
+#print(data['Outcome'].value_counts())
+
+''' using upsampling to balance data '''
+data_majority = data[data['Outcome'] == 0]
+data_minority = data[data['Outcome'] == 1]
+upsample = resample(data_minority, replace=True, n_samples=500, random_state=42)   
+data = pd.concat([data_majority, upsample])
+
+''' checking if data is balanced '''
+sns.countplot(data['Outcome'])
+#print(data['Outcome'].value_counts())
+
+''' checking Co-relation '''
