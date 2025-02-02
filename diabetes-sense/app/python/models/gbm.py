49018e018077a19d9928dd48b02b94be46b1f57b
TayyebaSadaq <official.tayyeba@gmail.com>
2025-02-02 17:34:42 +0000

    implemented base gbm model

--- diabetes-sense/app/python/models/gbm.py
+++ diabetes-sense/app/python/models/gbm.py
@@ -1,34 +1,39 @@
-import joblib
-import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import GridSearchCV
-from models.preprocessing import preprocess_data
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_digits
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from lime import lime_tabular
 
-# Train Gradient Boosting model and save it
-def train_gbm_model():
-    x_train, x_test, y_train, y_test = preprocess_data('app/data/pima-indians-diabetes.csv')
-    param_grid = {
-        'n_estimators': [50, 100, 200],
-        'learning_rate': [0.01, 0.1, 0.2],
-        'max_depth': [3, 5, 7],
-        'min_samples_split': [2, 5, 10],
-        'min_samples_leaf': [1, 2, 4],
-    }
-    model = GridSearchCV(GradientBoostingClassifier(random_state=42), param_grid, cv=5).fit(x_train, y_train)
-    joblib.dump(model, 'app/model/gbm_classifier.pkl')
-    print("Gradient Boosting model trained and saved!")
+# setting SEED for reproducibility 
+SEED = 23
 
-# Predict using the trained model
-def predict_gbm(input_data):
-    model = joblib.load('app/model/gbm_classifier.pkl')
-    features = pd.DataFrame([input_data], columns=[
-        'Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 
-        'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age'
-    ])
-    prediction = model.predict(features)
-    probability = model.predict_proba(features)
-    return prediction, probability
+# importing dataset
+data = pd.read_csv(r"C:\Users\Tayyeba\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\pima.csv")
 
-# Trigger training when script runs
-if __name__ == '__main__':
-    train_gbm_model()
+# Define features and target variable
+X = data[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']] # Features
+y = data['Outcome'] # Target variable
+
+# splitting dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=SEED)
+
+# instantiate GBM Classifier 
+gbc = GradientBoostingClassifier(n_estimators = 300,
+                                 learning_rate = 0.05,
+                                 random_state = 100,
+                                 max_features = 8)
+
+# fit model to training data
+gbc.fit(X_train, y_train)
+
+# predict on test data
+y_pred = gbc.predict(X_test)
+
+# accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Gradient Boosting Classifier Accuracy: {accuracy:.2f}")
+
+### LIME IMPLEMENTATION
