104cca351619bd7e3f4ccfa0cab5c7ef225a2c2b
Tayyeba <official.tayyeba@gmail.com>
2025-02-16 18:04:32 +0000

    working on backend
    updated preprocessing to clean data, handle outliers etc. edited model making  after standardising the data (removed for loop and individually coded models

--- diabetes-sense/app/python/ml_model.py
+++ diabetes-sense/app/python/ml_model.py
@@ -13,12 +13,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 
 from lime.lime_tabular import LimeTabularExplainer
-import shap
-
 import warnings
 
+# Suppress all warnings
+warnings.filterwarnings('ignore')
+
 ### IMPORTING PREPROCESSED DATA
-data = pd.read_csv(r"C:\Users\Tayyeba\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\preprocessed_pima.csv")
+data = pd.read_pickle(r"C:\Users\tayye\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\preprocessed_pima.pkl")
 
 ### define the features and target variables
 X = data[['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']] # Features
@@ -27,36 +28,91 @@ y = data['Outcome'] # Target variable
 ### Splitting the data into training and testing sets (70% training, 30% testing)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-### CREATING CLASSIFIERS FOR MODELS
-models = [LogisticRegression(max_iter=500), RandomForestClassifier(), GradientBoostingClassifier()]
+### standardizing the data
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-for each in models:
-    each.fit(X_train, y_train)
-    y_pred = each.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    classification_rep = classification_report(y_test, y_pred)
-    print(f"{each} Model")
-    print(f"Accuracy: {accuracy:.2f}")
-    print("\nClassification Report:\n", classification_rep)
-    
-    # sample prediction
-    sample = X_test.iloc[0:1] # Keep as DataFrame to match model input format
-    prediction = each.predict(sample)
-    sample_dict = sample.iloc[0].to_dict()
-    print(f"\n\nSample Patient: {sample_dict}")
-    print(f"Predicted Diagnosis: {'Diabetic' if prediction[0] == 1 else 'Not Diabetic'}")
-    
-    ## LIME EXPLANATION
-    explainer = LimeTabularExplainer(X_train.values, mode="classification", feature_names=X_train.columns)
-    exp = explainer.explain_instance(sample.values[0], each.predict_proba, num_features=8)
-    exp.show_in_notebook(show_table=True)
+### LOGISTIC REGRESSION MODEL
+lr = LogisticRegression(max_iter=500)
+lr.fit(X_train_scaled, y_train)
+y_pred_lr = lr.predict(X_test_scaled)
+accuracy_lr = accuracy_score(y_test, y_pred_lr)
+classification_rep_lr = classification_report(y_test, y_pred_lr)
+print(f"Logistic Regression Model")
+print(f"Accuracy: {accuracy_lr:.2f}")
+print("\nClassification Report:\n", classification_rep_lr)
 
-    ## SHAP EXPLANATION
-    ### not yet implemented ###
-    
+# Sample prediction for LR
+sample_lr = pd.DataFrame(X_test_scaled).iloc[0:1]
+prediction_lr = lr.predict(sample_lr)
+sample_dict_lr = sample_lr.iloc[0].to_dict()
+print(f"\n\nSample Patient: {sample_dict_lr}")
+print(f"Predicted Diagnosis: {'Diabetic' if prediction_lr[0] == 1 else 'Not Diabetic'}")
+
+# LIME For Logistic Regression  (which use scaled data)
+explainer_lr = LimeTabularExplainer(X_train.values, mode="classification", feature_names=X_train.columns)
+exp_lr = explainer_lr.explain_instance(sample_lr.values[0], lr.predict_proba, num_features=8)
+exp_lr.show_in_notebook(show_table=True)
+
+
+# Ensure X_train and X_test are DataFrames - for RF model ONLY
+X_train = pd.DataFrame(X_train)
+X_test = pd.DataFrame(X_test)
+
+### RANDOM FOREST MODEL
+rf = RandomForestClassifier()
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict(X_test)
+accuracy_rf = accuracy_score(y_test, y_pred_rf)
+classification_rep_rf = classification_report(y_test, y_pred_rf)
+print(f"Random Forest Model")
+print(f"Accuracy: {accuracy_rf:.2f}")
+print("\nClassification Report:\n", classification_rep_rf)
+
+# Sample prediction for RF
+sample_rf = X_test.iloc[0:1]  # Using iloc to get a single row as a DataFrame
+prediction_rf = rf.predict(sample_rf)
+sample_dict_rf = sample_rf.iloc[0].to_dict()
+print(f"\n\nSample Patient: {sample_dict_rf}")
+print(f"Predicted Diagnosis: {'Diabetic' if prediction_rf[0] == 1 else 'Not Diabetic'}")
+
+# LIME For Random Forest (which uses non-scaled data)
+explainer_rf = LimeTabularExplainer(X_train.values, mode="classification", feature_names=X_train.columns)
+exp_rf = explainer_rf.explain_instance(sample_rf.values[0], rf.predict_proba, num_features=8)
+exp_rf.show_in_notebook(show_table=True)
+
+
+### GRADIENT BOOSTING MODEL
+gmb = GradientBoostingClassifier()
+gmb.fit(X_train_scaled, y_train)
+y_pred_gmb = gmb.predict(X_test_scaled)
+accuracy_gmb = accuracy_score(y_test, y_pred_gmb)
+classification_rep_gmb = classification_report(y_test, y_pred_gmb)
+print(f"Gradient Boosting Model")
+print(f"Accuracy: {accuracy_gmb:.2f}")
+print("\nClassification Report:\n", classification_rep_gmb)
+
+# Sample prediction for GMB
+sample_gmb = pd.DataFrame(X_test_scaled).iloc[0:1]
+prediction_gmb = gmb.predict(sample_gmb)
+sample_dict_gmb = sample_gmb.iloc[0].to_dict()
+print(f"\n\nSample Patient: {sample_dict_gmb}")
+print(f"Predicted Diagnosis: {'Diabetic' if prediction_gmb[0] == 1 else 'Not Diabetic'}")
+
+# LIME For Gradient Boosting (which uses scaled data)
+explainer_gmb = LimeTabularExplainer(X_train.values, mode="classification", feature_names=X_train.columns)
+exp_gmb = explainer_gmb.explain_instance(sample_gmb.values[0], gmb.predict_proba, num_features=8)
+exp_gmb.show_in_notebook(show_table=True)
+
+
 ### SAVING MODELS    
+models = [lr, rf, gmb]
 model_names = ["logistic_regression.pkl", "random_forest.pkl", "gradient_boosting.pkl"]
-model_folder = r"C:\Users\Tayyeba\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\models"
+
+model_folder = r"C:\Users\tayye\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\models"
+
+# Save each model
 for each, name in zip(models, model_names):
     joblib.dump(each, model_folder + "\\" + name)
     print(f"{name} saved successfully at {model_folder}")
--- diabetes-sense/app/python/preprocessing.py
+++ diabetes-sense/app/python/preprocessing.py
@@ -4,9 +4,33 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
-import csv
+import pickle
 
-data = pd.read_csv(r"C:\Users\Tayyeba\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\pima.csv")
+data = pd.read_csv(r"C:\Users\tayye\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\pima.csv")
+
+
+# checking functions - multi use
+def round(data):
+    round=(data.isin([0, None, np.nan]).sum() / data.shape[0] * 100).round(2)
+    return round
+    
+# IQR Method
+def remove_outliers_iqr(df, column):
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+    lb = Q1 - 1.5 * IQR
+    ub = Q3 + 1.5 * IQR
+    return df[(df[column] >= lb) & (df[column] <= ub)]
+
+# Z-score Method - not used as the data has lots of outliers 
+def remove_outliers_zscore(df, column, threshold=3):
+    mean = df[column].mean()
+    std = df[column].std()
+    lb = mean - threshold * std
+    ub = mean + threshold * std
+    return df[(df[column] >= lb) & (df[column] <= ub)]
+
 # print(data.head())
 # data.info()
 # print(data.isnull().sum())
@@ -14,136 +38,62 @@ data = pd.read_csv(r"C:\Users\Tayyeba\Desktop\Diabetes-Prediction-using-Machine-
 ## STATISTICAL ANALYSIS
 # print(data.describe())
 
-## OUTLIERS
-fig, axs = plt.subplots(9, 1, dpi = 95, figsize = (7,17))
+## CHECK CATEGORICAL AND NUMERICAL COLUMNS
+categorical_columns = [col for col in data.columns if data[col].dtype == 'object']
+# print("Categorical Columns: ", categorical_columns)
+numerical_columns = [col for col in data.columns if data[col].dtype != 'object']
+# print("Numerical Columns: ", numerical_columns)
+
+
+## REMOVE UNWANTED COLUMNS
+# NOT YET DECIDED
+
+## CHECKING FOR MISSING DATA
+round(data)
+
+## HANDLING THE MISSING DATA
+# dropping glucose bp and bmi
+data = data.drop(data[data['Glucose'] == 0].index)
+data = data.drop(data[data['BloodPressure'] == 0].index)
+data = data.drop(data[data['BMI'] == 0].index)
+round(data)
+# median of skin thickness, pregnancies and insulin 
+data['SkinThickness'] = data['SkinThickness'].replace(0, data['SkinThickness'].median())
+data['Pregnancies'] = data['Pregnancies'].replace(0, data['Pregnancies'].median())
+data['Insulin'] = data['Insulin'].replace(0, data['Insulin'].median())
+round(data)
+
+## REMOVING OUTLIERS
+# check outliers for pregnancies, bmi, insulin, blood pressure
+fig, axs = plt.subplots(4, 1, dpi = 95, figsize = (7,17))
 i = 0
-for col in data.columns:
+for col in ['Pregnancies', 'BMI', 'Insulin', 'BloodPressure']:
     axs[i].boxplot(data[col], vert = False)
     axs[i].set_ylabel(col)
     i += 1
 # plt.show()
 
-# Identify the quartiles
-q1, q3 = np.percentile(data['Insulin'], [25, 75])
-# Calculate the interquartile range
-iqr = q3 - q1
-# Calculate the lower and upper bounds
-lower_bound = q1 - (1.5 * iqr)
-upper_bound = q3 + (1.5 * iqr)
-# Drop the outliers
-clean_data = data[(data['Insulin'] >= lower_bound) 
-                & (data['Insulin'] <= upper_bound)]
+# removing outliers (IQR method chosesn as it's more robust)
+data = remove_outliers_iqr(data, 'BMI')
+# data_z = remove_outliers_zscore(data, 'BMI')
+data = remove_outliers_iqr(data, 'Insulin')
+# data_z = remove_outliers_zscore(data, 'Insulin')
+data = remove_outliers_iqr(data, 'BloodPressure')
+# data_z = remove_outliers_zscore(data, 'BloodPressure')
+data = remove_outliers_iqr(data, 'Pregnancies')
+# data_z = remove_outliers_zscore(data, 'Pregnancies')
 
-
-# Identify the quartiles
-q1, q3 = np.percentile(clean_data['Pregnancies'], [25, 75])
-# Calculate the interquartile range
-iqr = q3 - q1
-# Calculate the lower and upper bounds
-lower_bound = q1 - (1.5 * iqr)
-upper_bound = q3 + (1.5 * iqr)
-# Drop the outliers
-clean_data = clean_data[(clean_data['Pregnancies'] >= lower_bound) 
-                        & (clean_data['Pregnancies'] <= upper_bound)]
-
-
-# Identify the quartiles
-q1, q3 = np.percentile(clean_data['Age'], [25, 75])
-# Calculate the interquartile range
-iqr = q3 - q1
-# Calculate the lower and upper bounds
-lower_bound = q1 - (1.5 * iqr)
-upper_bound = q3 + (1.5 * iqr)
-# Drop the outliers
-clean_data = clean_data[(clean_data['Age'] >= lower_bound) 
-                        & (clean_data['Age'] <= upper_bound)]
-
-
-# Identify the quartiles
-q1, q3 = np.percentile(clean_data['Glucose'], [25, 75])
-# Calculate the interquartile range
-iqr = q3 - q1
-# Calculate the lower and upper bounds
-lower_bound = q1 - (1.5 * iqr)
-upper_bound = q3 + (1.5 * iqr)
-# Drop the outliers
-clean_data = clean_data[(clean_data['Glucose'] >= lower_bound) 
-                        & (clean_data['Glucose'] <= upper_bound)]
-
-
-# Identify the quartiles
-q1, q3 = np.percentile(clean_data['BloodPressure'], [25, 75])
-# Calculate the interquartile range
-iqr = q3 - q1
-# Calculate the lower and upper bounds
-lower_bound = q1 - (0.75 * iqr)
-upper_bound = q3 + (0.75 * iqr)
-# Drop the outliers
-clean_data = clean_data[(clean_data['BloodPressure'] >= lower_bound) 
-                        & (clean_data['BloodPressure'] <= upper_bound)]
-
-
-# Identify the quartiles 
-q1, q3 = np.percentile(clean_data['BMI'], [25, 75])
-# Calculate the interquartile range
-iqr = q3 - q1
-# Calculate the lower and upper bounds
-lower_bound = q1 - (1.5 * iqr)
-upper_bound = q3 + (1.5 * iqr)
-# Drop the outliers
-clean_data = clean_data[(clean_data['BMI'] >= lower_bound) 
-                        & (clean_data['BMI'] <= upper_bound)]
-
-
-# Identify the quartiles
-q1, q3 = np.percentile(clean_data['DiabetesPedigreeFunction'], [25, 75])
-# Calculate the interquartile range
-iqr = q3 - q1
-# Calculate the lower and upper bounds
-lower_bound = q1 - (1.5 * iqr)
-upper_bound = q3 + (1.5 * iqr)
-
-# Drop the outliers
-clean_data = clean_data[(clean_data['DiabetesPedigreeFunction'] >= lower_bound) 
-                        & (clean_data['DiabetesPedigreeFunction'] <= upper_bound)]
-
-fig, axs = plt.subplots(9, 1, dpi = 95, figsize = (7,17))
+# checking outliers after removing
+fig, axs = plt.subplots(4, 1, dpi = 95, figsize = (7,17))
 i = 0
-for col in clean_data.columns:
-    axs[i].boxplot(clean_data[col], vert = False)
+for col in ['Pregnancies', 'BMI', 'Insulin', 'BloodPressure']:
+    axs[i].boxplot(data[col], vert = False)
     axs[i].set_ylabel(col)
     i += 1
-# plt.show()
+plt.show()
 
-corr = clean_data.corr()
-plt.figure(dpi = 130)
-sns.heatmap(clean_data.corr(), annot = True, fmt = '.2f')
-# plt.show()
+print(data.duplicated().sum()) # duplicate checking
 
-## OUTCOME PROPORTIONALITY
-plt.pie(clean_data.Outcome.value_counts(), 
-        labels= ['Diabetes', 'Not Diabetes'], 
-        autopct='%.f', shadow=True)
-plt.title('Outcome Proportionality')
-# plt.show()
-
-## SEPARATE INDEPENDENT FEATURES AND TARGET VARIABLES
-X = clean_data.drop(columns = ['Outcome'])
-Y = clean_data.Outcome
-
-## NORMALISATION - function since it's only needed for the neural network
-def normalisation(X):
-    scaler = MinMaxScaler(feature_range = (0, 1))
-    
-    rescaledX = scaler.fit_transform(X)
-    return rescaledX[:5]
-    
-print(normalisation(X))
-
-# Save as CSV
-clean_data.to_csv(r"C:\Users\Tayyeba\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\preprocessed_pima.csv", index=False)
-
-# Save as Pickle (for faster loading)
-import pickle
-with open(r"C:\Users\Tayyeba\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\preprocessed_pima.pkl", "wb") as f:
-    pickle.dump(clean_data, f)
+# # Save as Pickle (for faster loading)
+with open(r"C:\Users\tayye\Desktop\Diabetes-Prediction-using-Machine-Learning-and-Explainable-AI-Techniques\diabetes-sense\app\data\preprocessed_pima.pkl", "wb") as f:
+    pickle.dump(data, f)
